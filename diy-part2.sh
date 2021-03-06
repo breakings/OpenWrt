3c024636af905706b4a5e472879780c30a5f36c0
breakings <skygmin@gmail.com>
2021-03-06 22:08:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,9 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
 #
 
 # Modify default IP
