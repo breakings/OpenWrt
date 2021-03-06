714bbf298856bad33af67d30b1064c21a098f14a
breakings <skygmin@gmail.com>
2021-03-06 08:06:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-svn checkout https://github.com/openwrt/packages/trunk/utils/libnetwork packages/utils
