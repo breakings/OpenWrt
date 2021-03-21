eb75fb8ed4b0152c5832acb6ef278b7fd18f28f3
breakings <skygmin@gmail.com>
2021-03-21 20:04:09 +0800

    Update openwrt-21.02-diy-part2.sh

--- openwrt-21.02-diy-part2.sh
+++ openwrt-21.02-diy-part2.sh
@@ -13,6 +13,28 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+<<'COMMENT'
+latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p)"
+curl -LO "https://github.com/openwrt/openwrt/archive/${latest_release}"
+mkdir openwrt_back
+shopt -s extglob 
+tar zxvf ${latest_release}  --strip-components 1 -C ./openwrt_back
+rm -f ${latest_release}
+git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt_new
+rm -f ./openwrt_new/include/version.mk
+rm -f ./openwrt_new/include/kernel-version.mk
+rm -f ./openwrt_new/package/base-files/image-config.in
+rm -rf ./openwrt_new/target/linux/*
+cp -f ./openwrt_back/include/version.mk ./openwrt_new/include/version.mk
+cp -f ./openwrt_back/include/kernel-version.mk ./openwrt_new/include/kernel-version.mk
+cp -f ./openwrt_back/package/base-files/image-config.in ./openwrt_new/package/base-files/image-config.in
+cp -rf ./openwrt_back/target/linux/* ./openwrt_new/target/linux/
+mkdir openwrt
+cp -rf ./openwrt_new/* ./openwrt/
+COMMENT
+
+git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
+
 #移除不用软件包    
 rm -rf feeds/packages/libs/libgd-full
 rm -rf feeds/luci/collections/luci-lib-docker
