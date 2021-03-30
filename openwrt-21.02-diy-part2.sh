a31bfd5a888d6db53d67cf7d1d12892e23f57836
breakings <skygmin@gmail.com>
2021-03-30 09:32:51 +0800

    Update openwrt-21.02-diy-part2.sh

--- openwrt-21.02-diy-part2.sh
+++ openwrt-21.02-diy-part2.sh
@@ -189,7 +189,9 @@ cp -f $GITHUB_WORKSPACE/PATCH/new/package/100-Implements-AES-and-GCM-with-ARMv8-
 #sed -i 's/Os/O3/g' include/target.mk
 sed -i 's,-mcpu=generic,-march=armv8-a+crypto+crc -mabi=lp64,g' include/target.mk
 
+# Transmission
 sed -i 's/发送/Transmission/g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
+sed -i '/"file": {/a\\t\t\t\t"/etc/passwd": [ "read" ],\n\t\t\t\t"/etc/group": [ "read" ],' /usr/share/rpcd/acl.d/luci-app-transmission.json
 
 <<'COMMENT'
 #Vermagic
