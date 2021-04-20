31690ef418a2b9bc4adfb438aa75f627f07fbce4
breakings <skygmin@gmail.com>
2021-04-20 21:37:41 +0800

    update
    update files

---
+++ opt/docker/buildOfficial.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TAG=latest
+if [ ! -z "$1" ];then
+	TAG=$1
+fi
+
+TMPDIR=openwrt_rootfs
+OUTDIR=/opt/imgs/docker
+IMG_NAME=baltimore/openwrt-aarch64
+
+[ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
+
+mkdir -p "$TMPDIR"  && \
+gzip -dc openwrt-armvirt-64-default-rootfs-official.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
+cp -f patches/rc.local "$TMPDIR/etc/" && \
+cp -f patches/cpustat "$TMPDIR/usr/bin/" && chmod 755 "$TMPDIR/usr/bin/cpustat" && \
+cp -f patches/getcpu "$TMPDIR/bin/" && chmod 755 "$TMPDIR/bin/getcpu" && \
+chmod 755 "$TMPDIR/etc/init.d/flowoffload" && \
+#cat patches/luci-admin-status-index-html.patch | (cd "$TMPDIR/usr/lib/lua/luci/view/admin_status/" && patch -p0) && \
+rm -f "$TMPDIR/etc/bench.log" && \
+sed -e 's/\/opt/\/etc/' -i "${TMPDIR}/etc/config/qbittorrent" && \
+sed -e "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" -i "${TMPDIR}/etc/ssh/sshd_config" && \
+sss=$(date +%s) && \
+ddd=$((sss/86400)) && \
+sed -e "s/:0:0:99999:7:::/:${ddd}:0:99999:7:::/" -i "${TMPDIR}/etc/shadow" && \
+echo "17 3 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
+rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
+(cd "$TMPDIR" && tar cf ../openwrt-armvirt-64-default-rootfs.tar .) && \
+rm -f DockerImg-OpenwrtArm64-${TAG}.gz && \
+#docker build -t ${IMG_NAME}:${TAG} . && \
+docker build --platform=linux/arm64 -o type=docker -t ${IMG_NAME}:${TAG} . && \
+rm -f  openwrt-armvirt-64-default-rootfs.tar && \
+rm -rf "$TMPDIR" && \
+docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-official-aarch64-${TAG}.gz
