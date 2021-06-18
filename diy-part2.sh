18ccbb29b9896e32a0aa20a8e4c81e34680d2467
breakings <skygmin@gmail.com>
2021-06-19 04:14:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -124,8 +124,8 @@ git clone https://github.com/kevin-morgan/luci-theme-argon-dark.git package/luci
 #svn co https://github.com/openwrt/packages/trunk/utils/tini feeds/packages/utils/tini
 
 #删除docker无脑初始化教程
-sed -i '31,39d' package/lean/luci-app-docker/po/zh-cn/docker.po
-rm -rf lean/luci-app-docker/root/www
+#sed -i '31,39d' package/lean/luci-app-docker/po/zh-cn/docker.po
+#rm -rf lean/luci-app-docker/root/www
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
