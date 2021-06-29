10a3191e78e7a1fc56998624aa9ca4964163cb95
breakings <skygmin@gmail.com>
2021-06-30 06:01:48 +0800

    Update flowoffload.lua

--- general/luci-app-flowoffload/luasrc/controller/flowoffload.lua
+++ general/luci-app-flowoffload/luasrc/controller/flowoffload.lua
@@ -4,11 +4,8 @@ function index()
 	if not nixio.fs.access("/etc/config/flowoffload") then
 		return
 	end
-	local page
-	page = entry({"admin", "network", "flowoffload"}, cbi("flowoffload"), _("Turbo ACC Center"), 101)
-	page.i18n = "flowoffload"
-	page.dependent = true
 	
+	entry({"admin", "network", "flowoffload"}, cbi("flowoffload"), _("Turbo ACC Center"), 1001).dependent = true
 	entry({"admin", "network", "flowoffload", "status"}, call("action_status"))
 end
 
@@ -24,13 +21,16 @@ local function is_fullcone()
 	return luci.sys.call("[ `cat /sys/module/xt_FULLCONENAT/refcnt 2>/dev/null` -gt 0 ] 2>/dev/null") == 0
 end
 
+local function is_dns()
+	return luci.sys.call("pgrep dnscache >/dev/null") == 0
+end
+
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		run_state = is_running(),
 		down_state = is_bbr(),
-		up_state = is_fullcone()
+		up_state = is_fullcone(),
+		dns_state = is_dns()
 	})
 end
-
-
