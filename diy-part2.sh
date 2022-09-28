1cee8f3ea968648c89b1850804733e3385704349
breakings <skygmin@gmail.com>
2022-09-29 06:51:40 +0800

    update config

--- diy-part2.sh
+++ diy-part2.sh
@@ -258,8 +258,8 @@ sed -i "s/PKG_HASH:=.*/PKG_HASH:=2c1486ab7e6dad76fb34642cd4f91d533e5dfeec0ee5271
 #sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
 
 # docker-compose
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.11.1/g' feeds/packages/utils/docker-compose/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=16c5fa92bdabc1f39526e40319c0975e37246bc87a873c92bf960fece47e2c3e/g' feeds/packages/utils/docker-compose/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.11.2/g' feeds/packages/utils/docker-compose/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=592e712f568938046602c0d4c225bc3c333e2b77574634fa0f39a8c066d04561/g' feeds/packages/utils/docker-compose/Makefile
 
 # containerd
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.5.11/g' feeds/packages/utils/containerd/Makefile
@@ -898,8 +898,8 @@ sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/packages/utils/xz/Makefile
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=e076ba3439cb7cfc45b908c869f51a8c89f3c9ee9ee982fde28849c015e723a7/g' feeds/packages/utils/xz/Makefile
 
 # vim
-#rm -rf feeds/packages/utils/vim
-#cp -rf $GITHUB_WORKSPACE/general/vim feeds/packages/utils
+rm -rf feeds/packages/utils/vim
+cp -rf $GITHUB_WORKSPACE/general/vim feeds/packages/utils
 
 # python-packaging
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=21.3/g' feeds/packages/lang/python/python-packaging/Makefile
@@ -918,7 +918,7 @@ rm -rf feeds/packages/lang/python/python-jsonschema
 svn co https://github.com/openwrt/packages/trunk/lang/python/python-jsonschema feeds/packages/lang/python/python-jsonschema
 
 # jd
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' feeds/packages/utils/jd/Makefile
+#sed -i 's/PKG_HASH:=.*/PKG_HASH:=5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' feeds/packages/utils/jd/Makefile
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
